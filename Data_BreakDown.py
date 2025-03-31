4787bdaedb0a07edef9bef06d45b0a5158431ec2
UndeadCapybara <wudaniel424@gmail.com>
2025-03-31 00:15:42 -0400

    Update Data_BreakDown.py

--- Data_BreakDown.py
+++ Data_BreakDown.py
@@ -1,6 +1,11 @@
 import copy
 import math
 
+#Reads the PomonaData File and funnels the data into the x, y, and z vectors
 xdata  =[]
 ydata = []
 zdata = []
+
+f = open("PomonaData.txt")
+data = f.readlines()
+f.close()
